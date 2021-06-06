4b85cc8d81e70b8edf16b0db2bb1cad5eac1d410
Vitalii Ivanov <vitalii.ivanov@spryker.com>
2021-06-06 12:13:15 +0300

    mysql replication

---
+++ mysql/mysql-slave-1/start.sql
@@ -0,0 +1,6 @@
+#  STOP SLAVE;
+# CHANGE MASTER TO MASTER_HOST='mysql-master',
+#     MASTER_USER='slave-1', MASTER_PASSWORD='password',
+#     MASTER_LOG_FILE = 'mysql-bin.000002', MASTER_LOG_POS = 3;
+# START SLAVE;
+# SHOW SLAVE STATUS;
